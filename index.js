33005aea67bb3a889a8d7c4f574f831bee4cd629
Smaran Shaha <smaranshaha@gmail.com>
2024-09-11 21:15:29 +0600

    practice while loop and for loop

--- index.js
+++ index.js
@@ -19,8 +19,8 @@
 // const age = 18;
 // age >= 19 ? console.log("vote dioo") : console.log("gumai thako");
 
-let price = 500;
-const isLeader = true;
+// let price = 500;
+// const isLeader = true;
 
 // if (isLeader === true) {
 //   price = 0;
@@ -29,4 +29,84 @@ const isLeader = true;
 // }
 // console.log(price);
 
-price = isLeader === true ? 0 : price + 100;
+// price = isLeader === true ? 0 : price + 100;
+
+// const friends = ["tapu", "shimul", "rajes", "rajib", "krisno"];
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+// friends.push("shoron", "aurbo");
+// console.log(friends);
+// friends[1] = "somitro";
+// console.log(friends);
+
+// const array = [2, 8, 9];
+// const job = array.indexOf(7);
+// console.log(job);
+
+// const player = friends.concat(array);
+// console.log(player);
+
+// const fruits = ["appel", "orange", "mango", "pineapple", "banana"];
+
+// for (const fruit of fruits) {
+//   console.log(fruit);
+//   console.log("ami khabo");
+// }
+
+// const number = 7;
+// if (number < 10) {
+//   console.log("monre moto number ache");
+// }
+// console.log("moner moto number payni");
+
+// let num = 0;
+// while (num < 5) {
+//   console.log("djfjfd", num);
+//   num++;
+// }
+
+// let num = 0;
+// while (num <= 10) {
+//   console.log("fjdjf", num);
+//   num++;
+// }
+
+// let num = 1;
+// let sum = 0;
+// while (num <= 10) {
+//   console.log(num);
+//   sum = sum + num;
+//   console.log("sum", sum);
+//   num++;
+// }
+
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+
+//   if (num % 2 === 1) {
+//     console.log("even num", num);
+//   }
+//   num++;
+// }
+
+// for (let num = 0; num < 10; num++) {
+//   console.log(num);
+// }
+
+// for (let num = 0; num < 10; num = num + 2) {
+//   console.log(num);
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+for (let i = 0; i <= 30; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}

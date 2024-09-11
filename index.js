97622ba1d1decf24702e5c9ace082b1f3039f3e2
Smaran Shaha <smaranshaha@gmail.com>
2024-09-12 01:40:36 +0600

    revesed added

--- index.js
+++ index.js
@@ -163,10 +163,21 @@
 // const port = address.split("a");
 // console.log(port);
 
-// const sentence = "i am learning web dev";
+const sentence = "i am learning web dev";
 // let reverse = "";
 // for (const letter of sentence) {
 //   //   console.log(letter);
 //   reverse = letter + reverse;
 // }
 // console.log(reverse);
+let rev = "";
+for (let i = 0; i < sentence.length; i++) {
+  //   console.log(i);
+  //   console.log(sentence[i]);
+  const letter = sentence[i];
+  rev = letter + rev;
+}
+console.log(rev);
+
+const reversed = sentence.split("").reverse();
+console.log(reversed);
